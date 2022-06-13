a4c9a25889908008536d3dfb02b4e247b5a5d4d5
Tan Wei Shwin, Linus <linustws00@gmail.com>
2022-06-14 03:13:35 +0800

    Show startscreen

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Font from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import StartScreen from './src/screens/StartScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App = () => {
+  return (
+    <StartScreen />
+  );
+};
