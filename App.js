1fd72a65cc2940dc4622d6a8ca38c1353b63e251
Tan Wei Shwin, Linus <linustws00@gmail.com>
2022-06-21 19:31:49 +0800

    Renamed Startscreen to SplashScreen

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import * as Font from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
 
-import StartScreen from './src/screens/StartScreen';
+import SplashScreen from './src/screens/SplashScreen';
 
 export default App = () => {
   return (
-    <StartScreen />
+    <SplashScreen />
   );
 };
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React from 'react';
+import { globalStyles } from '../styles/Styles';
+import { COLOURS } from '../styles/Colours';
+
+const SplashScreen = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={styles.nectusTitle}>NectUS</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  nectusTitle: {
+    fontFamily: 'roboto',
+    fontWeight: 'bold',
+    marginTop: 30,
+    color: COLOURS.orange,
+    textAlign: 'center',
+    fontSize: 60,
+  },
+});
+
+export default SplashScreen;
