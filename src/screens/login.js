b4b67016c0a249c8ddd6e9c789410671122b07f3
Tan Wei Shwin, Linus <linustws00@gmail.com>
2022-06-28 01:22:51 +0800

    Style changes

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 import { globalStyles } from '../styles/global';
 import { colors } from '../styles/colors';
 
 const Login = () => {
+  const linkToNUS = () => console.log("hi");
   return (
     <View style={globalStyles.container}>
       <StatusBar style="auto" />
       <Text style={styles.nectusTitle}>NectUS</Text>
+      <View style ={styles.imageContainer}>
+        <Image
+          style={{
+            resizeMode: "contain",
+            height: 300,
+            width: 300
+            
+          }}
+          source={require("../../assets/connect.png")}
+        />
+      </View>
+      <TouchableOpacity onPress={linkToNUS}>
+      <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <Text style={styles.footer}>Connect and meet new friends from NUS</Text>
     </View>
   );
 };
@@ -20,6 +36,25 @@ const styles = StyleSheet.create({
     color: colors.orange,
     textAlign: 'center',
     fontSize: 80,
+  },
+  imageContainer: {
+    alignItems: 'center',
+  },
+  loginText: {
+    fontFamily: 'roboto-bold',
+    margin: 25,
+    backgroundColor: colors.orange,
+    textAlign: 'center',
+    fontSize: 30,
+    padding: 15,
+    borderRadius: 20,
+    overflow: 'hidden'
+  },
+  footer: {
+    fontFamily: 'roboto-bold',
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 20,
   },
 });
 
